41ca9e9748ea88131ed3fc8bb1466d038700973e
Carlos Cosming <carlos@kingly.cl>
2024-11-21 11:15:33 -0300

    ✨ feat: enhance dev and peer deps

--- src/eslint/base.js
+++ src/eslint/base.js
@@ -1,7 +1,11 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended', 'prettier'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
   plugins: ['@typescript-eslint/eslint-plugin'],
   parser: '@typescript-eslint/parser',
   ignorePatterns: [
