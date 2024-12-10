77a1ed341723a83f3b16eb3d8247423d4feaae20
Carlos Cosming <carlos@kingly.cl>
2024-12-10 18:26:41 -0300

    ✨ feat: update config files for jest and typescript

--- src/index.mjs
+++ src/index.mjs
@@ -2,5 +2,6 @@ export { default as playwright } from './lib/playwright';
 export { default as tailwind } from './lib/tailwind';
 export { default as eslintNextjs } from './lib/eslint/nextjs.mjs';
 export { default as eslintNestjs } from './lib/eslint/nestjs.mjs';
-export { default as jestNextjs } from './lib/jest/nextjs';
 export { default as jestNestjs } from './lib/jest/nestjs';
+export { default as jestNextjsApp } from './lib/jest/nextjs.app.js';
+export { default as jestNextjsLib } from './lib/jest/nextjs.lib';
--- src/lib/tailwind.ts
+++ src/lib/tailwind.ts
@@ -8,10 +8,6 @@ const config: Omit<Config, 'content'> = {
         background: 'var(--background)',
         foreground: 'var(--foreground)',
       },
-      fontFamily: {
-        sans: ['var(--font-geist-sans)'],
-        mono: ['var(--font-geist-mono)'],
-      },
     },
   },
   plugins: [],
