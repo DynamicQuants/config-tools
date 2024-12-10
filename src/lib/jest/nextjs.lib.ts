ffb547e75c3449195d79c99557a39415041dccae
Carlos Cosming <carlos@kingly.cl>
2024-12-09 23:52:10 -0300

    ✨ feat: add jest config file for nextjs-lib

---
+++ src/lib/jest/nextjs.lib.ts
@@ -0,0 +1,21 @@
+import type { Config } from 'jest';
+
+import base from './base';
+
+const config: Config = {
+  ...base,
+  transform: {
+    '^.+\\.[tj]sx?$': [
+      '@swc/jest',
+      {
+        jsc: {
+          parser: { syntax: 'typescript', tsx: true },
+          transform: { react: { runtime: 'automatic' } },
+        },
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+};
+
+export default config;
