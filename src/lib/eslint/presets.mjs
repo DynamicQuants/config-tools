6470c0d5a8029643a73c0c896f32ecf78c2ca1b4
Carlos Cosming <carlos@kingly.cl>
2024-12-23 22:48:19 -0300

    🐛 fix: solve eslint error in presets config

--- src/lib/eslint/presets.mjs
+++ src/lib/eslint/presets.mjs
@@ -1,12 +1,18 @@
+import tseslint from 'typescript-eslint';
+
 /**
  * @type {import('@eslint/eslintrc').Config[]}
  */
-export default [
-  {
-    files: ['**/*.ts', '**/*.tsx'],
-    rules: {
-      '@typescript-eslint/consistent-type-imports': 'error',
-      '@typescript-eslint/consistent-type-exports': 'error',
+export default tseslint.config({
+  languageOptions: {
+    parserOptions: {
+      projectService: true,
+      tsconfigRootDir: import.meta.dirname,
     },
   },
-];
+  files: ['**/*.ts', '**/*.tsx'],
+  rules: {
+    '@typescript-eslint/consistent-type-imports': 'error',
+    '@typescript-eslint/consistent-type-exports': 'error',
+  },
+});
